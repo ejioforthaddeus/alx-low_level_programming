5a3f8df6036bc13bd97be9256f02cf1797d4b227
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2022-12-08 13:38:18 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- *
- * * main - Entry points
- *
- * * Return: Success (0)
- *
- *  */
+* main - Entry points
+* Return: Success (0)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
