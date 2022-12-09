e47298ed4c1d4df441f9ed35760c1f10c8b6261c
root <ejioforthaddeus037@gmail.com>
2022-12-09 06:29:25 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *main - Entry point
+ *
+ * *Description: prints numbers from 0-9
+ *
+ * *followed by new line
+ *
+ * *Return: Success (0)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+		int num = 0;
+
+
+
+			while (num < 10)
+
+						printf("%d", num++);
+
+				putchar('\n');
+
+					return (0);
+
+}
