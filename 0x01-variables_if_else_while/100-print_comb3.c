c7b7239cd8f0f88c6bc591b702950bc20def7971
root <ejioforthaddeus037@gmail.com>
2022-12-09 06:45:59 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints numbers between 01 to 89.
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int i, e, c;
+
+
+
+			i = 48;
+
+				e = 49;
+
+					c = 0;
+
+						while (e < 58)
+
+								{
+
+											if (c == 1)
+
+														{
+
+																		putchar(',');
+
+																					putchar(' ');
+
+																							}
+
+													putchar(i);
+
+															putchar(e++);
+
+																	c = 1;
+
+																			if (e == 58)
+
+																						{
+
+																										i++;
+
+																													e = i + 1;
+
+																																if (i == 58)
+
+																																				{
+
+																																									break;
+
+																																												}
+
+																																		}
+
+																				}
+
+							putchar('\n');
+
+								return (0);
+
+}
