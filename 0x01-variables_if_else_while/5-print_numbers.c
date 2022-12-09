47928933d8f0e3613902e3e20f0162199adb5a35
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2022-12-09 15:30:07 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * *main - Entry point
- *
- * *Description: prints numbers from 0-9
- *
- * *followed by new line
- *
- * *Return: Success (0)
- *
- * */
-
-
+*main - Entry point
+*Description: prints numbers from 0-9
+*followed by new line
+*Return: Success (0)
+*/
 
 int main(void)
-
 {
+	int num = 0;
 
-		int num = 0;
-
-
-
-			while (num < 10)
-
-						printf("%d", num++);
-
-				putchar('\n');
-
-					return (0);
-
+	while (num < 10)
+		printf("%d", num++);
+	putchar('\n');
+	return (0);
 }
