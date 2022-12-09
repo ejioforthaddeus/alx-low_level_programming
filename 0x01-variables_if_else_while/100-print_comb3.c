160b12d72771cba5db1799b9446e4d6643d5da41
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2022-12-09 15:47:10 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,73 +1,37 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints numbers between 01 to 89.
  *
- *  * main - Prints numbers between 01 to 89.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int i, e, c;
 
-		int i, e, c;
-
-
-
-			i = 48;
-
-				e = 49;
-
-					c = 0;
-
-						while (e < 58)
-
-								{
-
-											if (c == 1)
-
-														{
-
-																		putchar(',');
-
-																					putchar(' ');
-
-																							}
-
-													putchar(i);
-
-															putchar(e++);
-
-																	c = 1;
-
-																			if (e == 58)
-
-																						{
-
-																										i++;
-
-																													e = i + 1;
-
-																																if (i == 58)
-
-																																				{
-
-																																									break;
-
-																																												}
-
-																																		}
-
-																				}
-
-							putchar('\n');
-
-								return (0);
-
+	i = 48;
+	e = 49;
+	c = 0;
+	while (e < 58)
+	{
+		if (c == 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(i);
+		putchar(e++);
+		c = 1;
+		if (e == 58)
+		{
+			i++;
+			e = i + 1;
+			if (i == 58)
+			{
+				break;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
