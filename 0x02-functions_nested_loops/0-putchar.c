6f29c489968bda3010546e384bfe52c491425239
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2022-12-14 11:36:20 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
+/**
+*main - Entry point of the program
+*Description:  program that prints _putchar, followed by a new line.
+*Return: Success (0)
+*/
+
 int main(void)
 {
-	char *mychar = "_putchar";
+	char mychars[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i = 0;
 
-	while(i < 8)
+	while (i < 8)
 	{
-	_putchar(mychar[i]);
-	i++;
-	}
+		_putchar(mychars[i]);
+		i++;
 
+	}
+	_putchar('\n');
 	return (0);
 }
