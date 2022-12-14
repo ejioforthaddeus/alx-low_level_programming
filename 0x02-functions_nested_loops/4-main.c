e6653fd8d4c3f007c720c700736e8e06bc565154
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2022-12-14 12:37:12 +0100

    Create 4-main.c

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*Description: Calls the function _islower()
+*Return: Success (0)
+*/
+
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+Footer
+© 2022 GitHub,
