d403a67b0034d9539bdd9ebb76ef21a998b9f924
eya thaddeus ejiofor <109828056+ejioforthaddeus@users.noreply.github.com>
2023-01-06 16:01:51 +0100

    Create 1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+**_memcpy - Copies memory location
+*@dest: destination
+*@src: source
+*@n: number of bytes
+*Return: Returns Destination
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
